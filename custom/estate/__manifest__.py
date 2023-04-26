64db3e78007174b490e85bed33da67cdff1fab12
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-04-26 15:20:50 +0200

    add views + menus

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'estate',
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml'
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ]
 }
