da1378c297ed01c8fe5748d9071a40e3108d3f35
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-04-27 11:04:21 +0200

    custom fields

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -1,16 +1,20 @@
 from odoo import models, fields
+from dateutil.relativedelta import relativedelta
 
 class Property(models.Model):
     _name = "estate.property"
     _description = "Property Model"
 
+    def _new_date_availability(self):
+        return fields.Date().today() + relativedelta(months=3)
+
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: self._new_date_availability())
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -18,4 +22,11 @@ class Property(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         selection = [('north', 'North'), ('south','South'), ('east','East'), ('west','West')]
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        selection = [('new', 'New'), ('offer received','Offer Received'), ('offer accepted','Offer Accepted'), ('sold','Sold'), ('canceled', 'Canceled')],
+        default="new",
+        copy=False
     )
