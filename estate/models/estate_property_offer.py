9a1368fbd5fd2b9077a21e181c7826750e70a4a8
nass22 <nacer.smr@gmail.com>
2023-12-21 21:14:19 +0100

    add validity + date_deadline in offer

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from datetime import timedelta, date
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -8,3 +9,24 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(string="Status", selection=[('acccepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                date = record.create_date.date()
+            else:
+                date = fields.Date.today()
+
+            record.date_deadline = date + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                date = record.create_date.date()
+            else:
+                date = fields.Date.today()
+
+            record.validity = (record.date_deadline - date).days
