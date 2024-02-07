7cac837a0062156432391a0322ef9832ed79255b
nass22 <nacer.smr@gmail.com>
2024-02-07 09:52:55 +0100

    add attributs & options

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,11 +1,9 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
     _description = "Type of property"
     _order = "sequence, name"
-    property_ids = fields.One2many("estate.property", "property_type_id")
-
     # SQL CONSTRAINTS
     _sql_constraints = [
         ('check_name', 'UNIQUE(name)', 'The name must be unique!')
@@ -13,3 +11,13 @@ class EstatePropertyType(models.Model):
 
     name = fields.Char(required=True)
     sequence = fields.Integer('Sequence', default=1)
+    property_ids = fields.One2many("estate.property", "property_type_id", string="Properties")
+    offer_ids = fields.One2many("estate.property.offer", "property_type_id", string="Offers")
+    offer_count = fields.Integer(string="Offers Count", compute="_compute_offer")
+
+    def _compute_offer(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
+            
+        
+        
