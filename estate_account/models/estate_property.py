82e498050d896853e9d274af3257d242bb5d76af
nass22 <nacer.smr@gmail.com>
2024-02-09 13:47:16 +0100

    create invoice

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -4,5 +4,20 @@ class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def sold_property(self):
-        print(super().sold_property)
+        res = super().sold_property()
+        journal = self.env["account.journal"].search([("type", "=", "sale")], limit=1)
+        # FONCTIONNE PAS!
+        # journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+
+        for prop in self:
+            self.env['account.move'].create(
+                {
+                    "partner_id": prop.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "journal_id": journal.id,
+                }
+            )
+
+        return res
+
     
